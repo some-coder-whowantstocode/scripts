a807c652840fd65632355ecf3b8172874a354b57
some-coder-whowantstocode <rb1542044@gmail.com>
2025-06-12 17:39:46 +0530

    add instructions

--- fileorganizer.sh
+++ fileorganizer.sh
@@ -11,3 +11,8 @@ files=($(find "$dir" -maxdepth 1 -type f))
 for file in "${files[@]}"; do
 	echo "$file"
 done
+
+# take input as an array for what extentions the user wants to create files
+# create assosiate array 
+# turn them into arrays after storing them 
+
