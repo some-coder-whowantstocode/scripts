716499dbc365a24c5bbd1e3a843f368b421e7f70
some-coder-whowantstocode <rohit19003@gmail.com>
2025-06-12 18:59:15 +0530

    add feature for user to input extentions

--- fileorganizer.sh
+++ fileorganizer.sh
@@ -2,14 +2,25 @@
 
 read -p "Enter directory name : "  dir
 
-#echo "files and sub directories inside : "
+#echo "files inside : "
 
 files=($(find "$dir" -maxdepth 1 -type f))
 
 #echo "${files[@]}"
 
+#for file in "${files[@]}"; do
+#echo "$file"
+#done
+
+read -p "Enter extentions you want to separate : " -a ext
+
+for i in "${ext[@]}"; do
+	mkdir "i"
+done
+
+
 for file in "${files[@]}"; do
-	echo "$file"
+echo "$file"
 done
 
 # take input as an array for what extentions the user wants to create files
